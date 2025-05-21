026d5c80d5635a2ea1be4c767bda192e1db7d17c
chrisvidal <chris@voilaah.com>
2025-05-21 13:19:57 +0200

    up

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "SkillUp Documentation",
   description: "Official SkillUp Global Documentation for features, backend users and API integration",
+  lang: 'en-US',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: {
@@ -23,18 +24,88 @@ export default defineConfig({
           { text: 'Guide', link: '/guide/', activeMatch: '/guide/' },
           { text: 'Development', link: '/development/', activeMatch: '/development/' }
       ],
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+    sidebar: {
+            '/guide/': [
+                {
+                    text: 'Introduction',
+                    collapsed: false,
+                    items: [
+                        { text: 'Getting Started', link: '/guide/' },
+                        { text: 'Installation', link: '/guide/introduction/installation' },
+                        { text: 'Configuration', link: '/guide/introduction/configuration' }
+                    ]
+                },
+                {
+                    text: 'Usage',
+                    collapsed: false,
+                    items: [
+                        { text: 'Courses', link: '/guide/usage/properties' },
+                        { text: 'Categories', link: '/guide/usage/categories' },
+                        { text: 'Reviews', link: '/guide/usage/reviews' },
+                        { text: 'Settings', link: '/guide/usage/settings' },
+                        { text: 'Going Live', link: '/guide/usage/going-live' },
+                    ]
+                },
+                {
+                    text: 'Components',
+                    collapsed: true,
+                    items: [
+                        { text: 'Course', link: '/guide/components/product' },
+                        { text: 'Courses', link: '/guide/components/products' },
+                        { text: 'CoursesFilter', link: '/guide/components/courses-filter' },
+                        { text: 'CourseReview', link: '/guide/components/course-reviews' },
+                        { text: 'MyAccount', link: '/guide/components/my-account' },
+                    ]
+                },
+                {
+                    text: 'Integration',
+                    collapsed: true,
+                    items: [
+                        { text: 'Introduction', link: '/guide/integration/introduction' },
+                        { text: 'Courses List', link: '/guide/integration/courses' },
+                        { text: 'Course Content', link: '/guide/integration/course' },
+                    ]
+                }
+            ],
+            '/development/': [
+                {
+                    text: 'Working with Mall',
+                    collapsed: false,
+                    items: [
+                        { text: 'Product Model', link: '/development/core/product-model' },
+                        { text: 'Variant Model', link: '/development/core/variant-model' },
+                        { text: 'Order Model', link: '/development/core/order-model' },
+                        { text: 'Cart Model', link: '/development/core/cart-model' },
+                        { text: 'Wishlist Model', link: '/development/core/wishlist-model' },
+                        { text: 'Payment Providers', link: '/development/core/payment-providers' },
+                        { text: 'Pricing Information', link: '/development/core/pricing-information' },
+                        { text: 'Price Bag', link: '/development/core/price-bag' },
+                        { text: 'Events', link: '/development/core/events' },
+                        { text: 'PDF', link: '/development/core/pdf' },
+                        { text: 'Integration', link: '/development/core/integration' },
+                        { text: 'Artisan Console', link: '/development/core/console-commands' },
+                    ]
+                },
+                {
+                    text: 'Extending SkillUp',
+                    collapsed: false,
+                    items: [
+                        { text: 'Unit Testing', link: '/development/extend/unit-testing' },
+                    ]
+                },
+            ],
+        },
+        socialLinks: [
+            {
+                icon: {
+                    svg: '<svg width="100%" height="100%" viewBox="0 0 9 9" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" fill="currentColor"><path d="M7.42,4.068c-0.13,-2.124 -2.444,-3.962 -3.397,-3.983c-0.953,-0.02 -2.735,2.23 -2.808,4.515c-0.067,2.144 1.762,3.614 2.549,3.8c0.022,-1.008 0.103,-4.721 0.117,-5.154c0.028,-0.885 0.072,-2.261 0.219,-2.307c0.085,-0.026 0.201,0.748 0.186,1.205c-0.003,0.087 -0.004,0.37 -0.004,0.771c0.16,-0.162 0.326,-0.331 0.428,-0.43c0.277,-0.273 0.4,-0.24 0.267,0.003c-0.082,0.149 -0.435,0.647 -0.694,1.008c0.002,0.727 0.005,1.642 0.008,2.495c0.366,-0.429 0.814,-0.942 1.05,-1.171c0.494,-0.478 0.546,-0.418 0.313,0.085c-0.143,0.307 -0.843,1.129 -1.36,1.776c0.003,0.757 0.006,1.393 0.006,1.697c1.386,-0.305 3.248,-2.224 3.12,-4.31" /></svg>'
+                },
+                link: 'https://octobercms.com/plugin/skillup'
+            },
+            {
+                icon: 'github',
+                link: 'https://github.com/voilaah/oc-skillup-plugin'
+            }
         ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
   }
 })
--- docs/guide/integration/courses.md
+++ docs/guide/integration/courses.md
@@ -7,9 +7,9 @@ SkillUp can import all or part of your courses directly from your platform.
 Your platform needs to provide the following API to retrieve your courses list.
 
 
-GET `https://external-platofrm.com/api/v1/courses`
+GET `https://your-external-platform.com/api/v1/courses`
 
-The API needs to follow the structure:
+The API needs to respond to the following specifications:
 
 ```json
 {
@@ -17,15 +17,15 @@ The API needs to follow the structure:
     "per_page": "10",
     "current_page": 2,
     "last_page": 2,
-    "next_page_url": "https://external-platofrm.com/api/v1/courses?page=3",
-    "prev_page_url": "https://external-platofrm.com/api/v1/courses?page=1",
+    "next_page_url": "https://your-external-platform.com/api/v1/courses?page=3",
+    "prev_page_url": "https://your-external-platform.com/api/v1/courses?page=1",
     "from": 1,
     "to": 10,
     "data" : [
         {
             "code": "001",
             "title": "A course title",
-            "thumbnail": "https://external-platofrm.com/storage/app/medias/courses/thumbnail/181120100340-hairdresser-3572053_1280.jpg",
+            "thumbnail": "https://your-external-platform.com/storage/app/medias/courses/thumbnail/181120100340-hairdresser-3572053_1280.jpg",
             "introduction": "Dolor commodo aliquip veniam labore amet elit. Laboris culpa quis id consequat voluptate officia tempor consectetur aliquip duis laborum occaecat.",
             "description": "Dolor commodo aliquip veniam labore amet elit. Laboris culpa quis id consequat voluptate officia tempor consectetur aliquip duis laborum occaecat.
 
@@ -34,7 +34,7 @@ The API needs to follow the structure:
             "outcomes": "Dolor commodo aliquip veniam labore amet elit. Laboris culpa quis id consequat voluptate officia tempor consectetur aliquip duis laborum occaecat.",
             "skills": "Dolor commodo aliquip veniam labore amet elit. Laboris culpa quis id consequat voluptate officia tempor consectetur aliquip duis laborum occaecat.",
             "inclusion": "Dolor commodo aliquip veniam labore amet elit. Laboris culpa quis id consequat voluptate officia tempor consectetur aliquip duis laborum occaecat.",
-            "url": "https://external-platofrm.com/courses/123456",
+            "url": "https://your-external-platform.com/courses/123456",
             "meta" : {
                 "duration": 150,
                 "organization": "Organization name",
